12dc3f5376111c371b5a8c3e31f6e796711b174c
bowenchiu <cbh@cameo.tw>
2019-09-29 17:23:40 +0800

    Add bootstrap stylish theme

--- bin/staticWeb.dart
+++ bin/staticWeb.dart
@@ -2,7 +2,21 @@ import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart';
 
 void mainStaticWeb() {
-  var handler = createStaticHandler('webix', defaultDocument: 'index.html');
+  webixDemo();
+  stylishDemo();
+}
 
+void webixDemo() {
+  var handler = createStaticHandler(
+      'webix/samples/', defaultDocument: 'index.html');
+  print('webixDemo web server at http://localhost:8080/');
   io.serve(handler, 'localhost', 8080);
+}
+
+void stylishDemo() {
+  var handler = createStaticHandler(
+      'startbootstrap-stylish-portfolio-gh-pages/',
+      defaultDocument: 'index.html');
+  print('stylishDemo web server at http://localhost:8081/');
+  io.serve(handler, 'localhost', 8081);
 }
