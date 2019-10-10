404c57ff7d0eda242da47f082eae1324da54d005
bowenchiu <cbh@cameo.tw>
2019-10-10 14:58:43 +0800

    + new a jclang.dart file to separate logic + find out that use $ to catenate string is different from + (+ is more suitable for map and map indexing to print out some values + print('staticWeb.dart/jclangAdd/mapJson:'+mapJson["cards"][2]["customFieldItems"][4]["idValue"]);

---
+++ bin/jclang.dart
@@ -0,0 +1,41 @@
+import 'trelloData.dart';
+
+/*
+add 客源 /cards/customFieldItems/idValue idCustomField=="*3a1b"
+add 起始日 /cards/customFieldItems/value/date
+add 案件名稱 /cards/name
+add 金額 /cards/customFieldItems/value/number
+add 交期 /cards/due
+add 人員 /cards/customFieldItems/idValue idCustomField=="*f3f0"
+add 優先次序 /cards/labels/color
+add 客戶 /cards/customFieldItems/value/text
+add 階段 /cards/idList
+add 產品類別 /cards/customFieldItems/idValue idCustomField=="*a129"
+replace /cards/customFieldItems/idValue
+/cards/customFields/options/value/text ../id
+*/
+
+void mainJclang(){
+  trelloDataHandler();
+}
+void trelloDataHandler(){
+  Map mapJson=getTrelloData();
+  String strJclangCommandRow='add 客源 /cards/customFieldItems/idValue idCustomField=="*3a1b"';
+  jclangCommandDispatcher(strJclangCommandRow,mapJson);
+}
+
+void jclangCommandDispatcher(String strJclangCommandRow, Map mapJson){
+  List lstJclangCommandRow=strJclangCommandRow.split(' ');
+  if(lstJclangCommandRow[0]=='add'){
+    jclangAdd(lstJclangCommandRow[1],lstJclangCommandRow[2],lstJclangCommandRow[3],mapJson);
+  }
+}
+
+void jclangAdd(strColumnName,strSelect,strWhere,Map mapJson){
+  //add 客源 /cards/customFieldItems/idValue idCustomField=="*3a1b"
+  //cards[] scan all list
+  //customFieldItems scan all list
+  //get idValue where idCustomField=="*3a1b" and skip all others
+  //mapJson["cards"][2]["customFieldItems"][4]["idValue"]
+  print('staticWeb.dart/jclangAdd/mapJson:'+mapJson["cards"][2]["customFieldItems"][4]["idValue"]);
+}
--- bin/staticWeb.dart
+++ bin/staticWeb.dart
@@ -1,44 +1,12 @@
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart';
-import 'trelloData.dart';
+import 'jclang.dart';
 
 void mainStaticWeb() {
   webixSamples();
   stylishDemo();
   adminLteDemo();
-  trelloDataHandler();
-}
-
-/*
-add 客源 /cards/customFieldItems/idValue idCustomField=="*3a1b"
-add 起始日 /cards/customFieldItems/value/date
-add 案件名稱 /cards/name
-add 金額 /cards/customFieldItems/value/number
-add 交期 /cards/due
-add 人員 /cards/customFieldItems/idValue idCustomField=="*f3f0"
-add 優先次序 /cards/labels/color
-add 客戶 /cards/customFieldItems/value/text
-add 階段 /cards/idList
-add 產品類別 /cards/customFieldItems/idValue idCustomField=="*a129"
-replace /cards/customFieldItems/idValue
-/cards/customFields/options/value/text ../id
-*/
-void trelloDataHandler(){
-  Map mapJson=getTrelloData();
-  String strJclangCommandRow='add 客源 /cards/customFieldItems/idValue idCustomField=="*3a1b"';
-  jclangCommandDispatcher(strJclangCommandRow,mapJson);
-
-}
-
-void jclangCommandDispatcher(String strJclangCommandRow, Map mapJson){
-  List lstJclangCommandRow=strJclangCommandRow.split(' ');
-  if(lstJclangCommandRow[0]=='add'){
-    jclangAdd(lstJclangCommandRow[1],lstJclangCommandRow[2],lstJclangCommandRow[3],mapJson);
-  }
-}
-
-void jclangAdd(strColumnName,strSelect,strWhere,Map mapJson){
-  print('staticWeb.dart/jclangAdd/');
+  mainJclang();
 }
 
 void webixSamples() {
