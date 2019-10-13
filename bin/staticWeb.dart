a5236e5d2c78dbb7b5f646ec84fa923a3971d765
WengRoy <wengroy@cameo.tw>
2019-10-13 16:04:21 +0800

    strHost

--- bin/staticWeb.dart
+++ bin/staticWeb.dart
@@ -28,6 +28,7 @@ void stylishDemo() {
 void adminLteDemo() {
   var handler =
   createStaticHandler('AdminLTE-master/', defaultDocument: 'index.html');
-  print('adminLteDemo at http://104.155.215.104:80/');
-  io.serve(handler, '104.155.215.104', 80);
+  String strHost='127.0.0.1';
+  print('adminLteDemo at http://$strHost:80/');
+  io.serve(handler, '$strHost', 80);
 }
