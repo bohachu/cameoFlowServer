76726d6dfdc03548e55ba7a00c40897fca3648f2
WengRoy <wengroy@cameo.tw>
2019-10-26 22:07:36 +0800

    . 已經可以排出顏色         if((j-1)%3==0){lstTable[i][j] = '🍏' * lstTable[i][j];}         if((j-1)%3==1){lstTable[i][j] = '🍋' * lstTable[i][j];}         if((j-1)%3==2){lstTable[i][j] = '🍅' * lstTable[i][j];}

--- bin/tableTrelloToThreeLight.dart
+++ bin/tableTrelloToThreeLight.dart
@@ -5,7 +5,52 @@ import 'dart:convert';
 
 void main() async {
   Map map = await readTableTrelloAjax();
-  Map mapThreeLight = convertTableTrelloToThreeLight(map);
+  Map mapThreeLight = tableTrelloToThreeLight(map);
+  List lstTable = mapThreeLightToTable(mapThreeLight);
+  String strJson = toLightSymbol(lstTable);
+  log('strJson:$strJson');
+}
+
+String toLightSymbol(List lstTable) {
+  //🍋🍀🍄🍏🍎🍊🍅🍑🍓🍒
+  for (int i = 0; i < lstTable.length; i++) {
+    for (int j = 0; j < lstTable[i].length; j++) {
+      if (lstTable[i][j] is int) {
+        if ((j - 1) % 3 == 0) {
+          lstTable[i][j] = '🍏' * lstTable[i][j];
+        }
+        if ((j - 1) % 3 == 1) {
+          lstTable[i][j] = '🍋' * lstTable[i][j];
+        }
+        if ((j - 1) % 3 == 2) {
+          lstTable[i][j] = '🍅' * lstTable[i][j];
+        }
+      }
+    }
+  }
+  return jsonEncode(lstTable);
+}
+
+List mapThreeLightToTable(Map mapThreeLight) {
+  List lstOut = [];
+  for (var e in mapThreeLight.entries) {
+    String strName = e.key;
+    List lstRow = [];
+    for (int i = 0; i < 16 * 3; i++) {
+      lstRow.add(0);
+    }
+    Map map14Steps = e.value;
+    for (String strStep in map14Steps.keys) {
+      String strStep2Digit = strStep.substring(0, 2);
+      int intStep2Digit = int.parse(strStep2Digit);
+      lstRow[(intStep2Digit - 1) * 3 + 0] = map14Steps[strStep]['green'] ?? 0;
+      lstRow[(intStep2Digit - 1) * 3 + 1] = map14Steps[strStep]['yellow'] ?? 0;
+      lstRow[(intStep2Digit - 1) * 3 + 2] = map14Steps[strStep]['pink'] ?? 0;
+    }
+    lstRow.insert(0, strName);
+    lstOut.add(lstRow);
+  }
+  return lstOut;
 }
 
 Future<Map> readTableTrelloAjax() async {
@@ -14,26 +59,25 @@ Future<Map> readTableTrelloAjax() async {
   return map;
 }
 
-Map convertTableTrelloToThreeLight(Map map) {
-  log('tableTrelloToThreeLight.dart:map:$map');
+Map tableTrelloToThreeLight(Map map) {
   List lst = map['data'];
+  Map mapNameStepLight = {};
   for (int i = 0; i < lst.length; i++) {
     String strName = lst[i][5];
     String strLight = lst[i][6];
     String strStep = lst[i][7];
     if (strName.length > 0) {
-      addCounter(strName, strStep, strLight);
+      addCounter(mapNameStepLight, strName, strStep, strLight);
     }
   }
-  log('tableTrelloToThreeLight.dart/mapNameStepLight:$mapNameStepLight');
+  log('mapNameStepLight:$mapNameStepLight');
+  return mapNameStepLight;
 }
 
-Map mapNameStepLight = {};
-
-void addCounter(String strName, String strStep, String strLight) {
+void addCounter(Map mapNameStepLight, String strName, String strStep, String strLight) {
   if (mapNameStepLight[strName] == null) {
     mapNameStepLight[strName] = {};
-    }
+  }
   if (mapNameStepLight[strName][strStep] == null) {
     mapNameStepLight[strName][strStep] = {};
   }
