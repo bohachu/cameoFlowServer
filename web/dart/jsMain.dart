1c2f2dd5a5fe56dfe5051e6a9f46d4684bf02e85
WengRoy <wengroy@cameo.tw>
2019-10-29 23:40:03 +0800

    == 20191029 23:00 20min storage test! 20191029 done 已經改善速度問題了： 3* 速度，能不能 cache 先讀取一次，再做動態更新？
    ~~ todo list ~~
    3* menu bar 介面拿掉不需要的部分，只留下想要的東西，並且中文化
    2* 1h 點點表：最上面有中文標題
    2* 1h 階段收入表：下面要有簽約前金額加總
    1* 1h 數字靠右而且三位一撇
    1* 2h known 運用 $('.header').load("header.html"); 側邊欄改可重用 系統面：adminlte 介面隱藏不需要的東西，可以用試算表隱藏 hidelang
    1* 1h known 直接改中文字串無法重用 系統面：adminlte 把英文介面改為中文介面，用一個試算表來改，不需要動到原本的 html: i18nlang

--- web/dart/jsMain.dart
+++ web/dart/jsMain.dart
@@ -1,11 +1,23 @@
 //import 'dart:js';
+import 'dart:convert';
+
 import 'tableTrello.dart';
 import 'tableDot.dart';
 import 'tableStepIncome.dart';
 import 'dart:js';
+import 'dart:html';
 
 void main() async {
+  if (window.localStorage['tableTrello'] != null) {
+    List lstTrelloTable = jsonDecode(window.localStorage['tableTrello']);
+    updateTables(lstTrelloTable);
+  }
   List lstTrelloTable = (await tableTrello())['data'];
+  window.localStorage['tableTrello'] = jsonEncode(lstTrelloTable);
+  updateTables(lstTrelloTable);
+}
+
+void updateTables(lstTrelloTable) {
   context.callMethod('funcTableTrello', [JsObject.jsify(lstTrelloTable)]);
   context.callMethod('funcTableDot', [JsObject.jsify(tableDot(lstTrelloTable))]);
   context.callMethod('funcTableStepIncome', [JsObject.jsify(tableStepIncome(lstTrelloTable))]);
