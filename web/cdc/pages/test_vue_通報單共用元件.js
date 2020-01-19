6b2ca5b810927907aa2784524a93e57dc806d4d6
WengRoy <wengroy@cameo.tw>
2020-01-20 07:15:21 +0800

    已經可以示範給所有人看 vue 共用元件

--- web/cdc/pages/test_vue_通報單共用元件.js
+++ web/cdc/pages/test_vue_通報單共用元件.js
@@ -1,5 +1,5 @@
 Vue.component('big_green', {
-  props: ['標題','顏色'],
+  props: ['標題', '顏色'],
   template: `
     <div>
       <h5 class="title font-weight-bold mt-3" :style="顏色"> {{標題}} 通報單位資料</h5>
@@ -7,19 +7,36 @@ Vue.component('big_green', {
   `
 })
 
-Vue.component('report_unit_name', {
-  props: ['black_title', 'default_text'],
+Vue.component('title_input', {
+  props: ['標題', '預設文字', '註解', '必填嗎', '灰底嗎', '搜尋嗎'],
   template: `
-            <div class="col-lg-6">
-              <div class="form-group">
-                <label class="fs-0" for="first-name"> {{ black_title }} <a href="codingMemo.html?strMemo=001 疾管署與卡米爾當面討論之後建議：按下放大鏡之後的醫院搜尋介面畫面已經做在精誠動態雛形，因此，可以用抓圖方式（彈出搜尋元件），精誠夥伴請提供給疾管署做確認。<br/>002 如果通報單位非醫院，要變成「非反灰」可自行輸入。">🔆</a></label>
-                <div class="d-flex">
-                  <input class="form-control text-secondary" style="background-color: #F2F2F2;" id="first-name"
-                         type="text" :value="default_text">
-                  <i class="fas fa-search" style="font-size: 20px; margin: auto 0; margin-left: 10px;"></i>                  
-                </div>
-              </div>
-            </div>
+    <div class="col-lg-6">
+      <div class="form-group">
+        <label class="fs-0" for="first-name"> {{標題}}
+            <required :必填嗎="必填嗎"></required> 
+            <memo :註解="註解"></memo>
+        </label>
+        <div class="d-flex">
+          <input v-if="灰底嗎=='true'" class="form-control text-secondary" style="background-color: #F2F2F2;" id="first-name" type="text" :value="預設文字">
+          <input v-if="灰底嗎=='false'" class="form-control text-secondary" id="first-name" type="text" :value="預設文字">
+          <i v-if="搜尋嗎=='true'" class="fas fa-search" style="font-size: 20px; margin: auto 0; margin-left: 10px;"></i>                  
+        </div>
+      </div>
+    </div>
+  `
+})
+
+Vue.component('required', {
+  props: ['必填嗎'],
+  template: `
+    <span v-if="必填嗎=='true'" style="color: red;">*</span>
+  `
+})
+
+Vue.component('memo', {
+  props: ['註解'],
+  template: `
+    <a v-if="註解.length > 0" :href="'codingMemo.html?strMemo='+註解">🔆</a>
   `
 })
 
